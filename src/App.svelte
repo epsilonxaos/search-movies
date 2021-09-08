611d26b7ea619088a0ce319fcbe77b4606af49ca
Jesus Gonzalez <wilberthg16@gmail.com>
2021-09-07 19:03:41 -0500

    Componentes y States

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,11 @@
 <script>
-	export let name;
+	// A diferencia de React no es necesario decirle a Svelte que haga
+	// export del componente, simplemente podemos hacer uso de el en cualquier
+	// momento, como en este ejemplo en donde instanciamos mas de un contador
+	import Counter from './Counter.svelte';
 </script>
 
-<main>
-	<h1>Hola {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<!-- En este contador pasamos en el props que inicie en 5 -->
+<Counter initialCounted={5} />
+<!-- Este toma el valor por defecto del props -->
+<Counter />
---
+++ src/Counter.svelte
@@ -0,0 +1,45 @@
+<script>
+    // Este export hace referencia a la props que podemos configurar desde fuera del componente
+	export let initialCounted = 0;
+
+    // Iniciamos un state del mismo componente
+	let contador = initialCounted;
+
+    // Funcion que incrementa el state de contador
+	function handleClick() {
+		contador ++;
+	}
+
+</script>
+
+<!-- Un dato muy interesante es que los estilos son propios del componente
+que quiero decir, los estilos instanciados en este componente seran exclusivos de este component
+y no se aplicaran a tags fuera del componente -->
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
+
+<main>
+	<h1>Contador:</h1>
+	<button on:click={handleClick}>Incrementar contador</button>
+	<br>
+	<span>{contador}</span>
+</main>
