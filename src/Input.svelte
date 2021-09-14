b759f2a35df04b1e557efc54679d5723fe6b8a94
Jesus Gonzalez <wilberthg16@gmail.com>
2021-09-13 23:06:45 -0500

    Condicionales reactivas y listas

--- src/Input.svelte
+++ src/Input.svelte
@@ -1,16 +1,56 @@
 <script>
+    import Movie from "./Movie.svelte";
+
     let value = '';
+    let loading = false;
     let response = [];
 
     const handleInput = (event) => value = event.target.value;
 
     $: if(value.length > 3) {
+        loading = true;
         fetch(`http://www.omdbapi.com/?s=${value}&apikey=8e534059`)
         .then(res => res.json())
         .then(respuestApi => {
-            response = respuestApi.Search;
+            response = respuestApi.Search || [];
+            loading = false;
         })
     }
 </script>
 
-<input type="text" value={value} on:input={handleInput}>
+<style>
+    table {
+        width: 100%;
+    }
+
+    td {
+        text-align: center;
+        padding: 10px;
+    }
+</style>
+
+<label for="search">Search movie:</label>
+<input type="text" id="search" value={value} on:input={handleInput}>
+<p>
+    {#if loading}
+        <strong>Cargando...</strong>
+    {:else}
+        <table>
+            <tr>
+                <th>Poster</th>
+                <th>Nombre</th>
+                <th>Tipo</th>
+                <th>Año</th>
+            </tr>
+            {#each response as {Title, Poster, Type, Year}}
+                <Movie Title={Title} Poster={Poster} Type={Type} Year={Year} />
+                {:else}
+                <tr>
+                    <td colspan="4"><b style="color: crimson">Sin resultados</b></td>
+                </tr>
+            {/each}
+        </table>
+    {/if}
+
+    
+</p>
---
+++ src/Movie.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let Title;
+    export let Poster;
+    export let Type;
+    export let Year;
+</script>
+
+
+<tr>
+    <td><img src={Poster} alt={Title} style="width: 200px; height: 250px; object-fit: cover;"></td>
+    <td>{Title}</td>
+    <td>{Type}</td>
+    <td>{Year}</td>
+</tr>
